c8f228c07c4349cc4ca8421c68639151dece977e
lun421 <gavinchen2002@gmail.com>
2024-05-25 21:31:57 +0800

    .0

--- tryingdemo.py
+++ tryingdemo.py
@@ -489,7 +489,7 @@ full_predictions[mem_days-1:mem_days-1+len(predicted_classes)] = predicted_class
 bt = Backtest(df, LSTMBasedStrategy, cash=10000, commission=.0425)
 results = bt.run()
 btplt = bt.plot()
-st.write(btplt)
+st.pyplot(btplt)
 
 
 
