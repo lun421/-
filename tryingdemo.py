843c1dd25bcce49bb8d6dab9e30367fb0ca443eb
lun421 <gavinchen2002@gmail.com>
2024-05-26 02:46:37 +0800

    new

--- tryingdemo.py
+++ tryingdemo.py
@@ -21,7 +21,7 @@ import contextlib
 
 
 
-st.title('Stock Backtesting Using LSTM Model')
+st.title('LSTM Model Stock Backtesting')
 st.header('期末專題報告Demo · 第12組')
 st.markdown('組長：  \n徐睿延 110099029')
 st.markdown(f"組員：  \n陳冠倫 110072250  \n陳亮廷 110072224  \n宋宇然 110072206  \n張稚婕 111042013  \n賀守聖 111042038")
@@ -67,22 +67,26 @@ st.code(code, language='python')
 #progress代表讀的時候不要有進度條
 #assign是pandas對於DF的一個添加方法，添加股票代碼進到DF裡面
 #選用2021-2023兩年資料進行建模，後續再用2023整年資料回測
-code = '''
-stocks = ['MSFT', 'AAPL', 'NVDA', 'AMZN', 'META', 'GOOG', 'BRK-B', 
-          'LLY', 'JPM', 'AVGO', 'XOM', 'UNH', 'V', 'TSLA', 'PG', 'MA', 
-          'JNJ', 'HD', 'MRK', 'COST', 'ABBV', 'CVX', 'CRM', 'BAC', 'NFLX']
-
-startdate = "2021-01-01"
-enddate = "2023-01-01"
-
-data = pd.concat(
-    [yf.download(stock, start=startdate, end=enddate, progress=False).assign(Stock=stock)
-     for stock in stocks],
-    axis=0
-)
-'''
 st.header("Reading Stock Data for Modeling", divider='grey')
-st.code(code, language='python')
+st.markdown("Choosing Top25 Companies in S&P500 by Index Weight")
+with st.echo():
+    # List of stocks
+    stocks = ['MSFT', 'AAPL', 'NVDA', 'AMZN', 'META', 'GOOG', 'BRK-B', 
+              'LLY', 'JPM', 'AVGO', 'XOM', 'UNH', 'V', 'TSLA', 'PG', 'MA', 
+              'JNJ', 'HD', 'MRK', 'COST', 'ABBV', 'CVX', 'CRM', 'BAC', 'NFLX']
+
+    # Start and end dates
+    startdate = "2021-01-01"
+    enddate = "2023-01-01"
+
+    data = pd.concat(
+        [yf.download(stock, start=startdate, end=enddate, progress=False).assign(Stock=stock)
+         for stock in stocks],
+        axis=0
+    )
+
+st.dataframe(data)
+
 
 #計算指標
 code = '''
@@ -204,22 +208,18 @@ st.code(code, language='python')
 #selected_features = ['Volume', 'RSI', 'Macdhist', 'EMA_3', 'EMA_50']
 code = '''
 indicator_model = RandomForestClassifier()
-indicator_model.fit(indicator_candidate_scaled, y)
-feature_importances = indicator_model.feature_importances_
-feature_names = indicator_candidate.columns
 
-features = list(zip(feature_names, feature_importances))
-sorted_features = sorted(features, key=lambda x: x[1], reverse=True)
-
-for feature, importance in sorted_features:
-    print(f'Feature: {feature}, Importance: {importance}')
-
-feature_data = data[selected_features+ ['Label']]
+# 使用 RFE 選取前五個最重要的特徵
+selector = RFE(indicator_model, n_features_to_select=5, step=1)
+selector = selector.fit(indicator_candidate_scaled, y)
+selected_features = selector.support_
+print("Selected features:", indicator_candidate.columns[selected_features])
+feature_data = data[indicator_candidate.columns[selected_features].tolist() + ['Label']]
 
 '''
 st.header("Picking Indicators using Random Forest", divider='grey')
 st.code(code, language='python')
-
+st.code(f"Selected features: Index(['Volume', 'RSI', 'ROC', 'ADX', 'EMA_9'], dtype='object')")
 
 #pre modeling
 code = '''
@@ -434,63 +434,60 @@ st.header("Backtesting Stocks", divider='grey')
 st.code(code, language='python')
 
 
-model = load_model("val05584_mem25_lstm1_dense2_unit256_dropout010_batch32.keras")
-def calculate_selected_indicators(data):
-    delta = data['Close'].diff()
-    up, down = delta.clip(lower=0), -delta.clip(upper=0)
-    ma_up = up.rolling(window=14).mean()
-    ma_down = down.rolling(window=14).mean()
-    data['RSI'] = 100 - (100 / (1 + ma_up / ma_down))
+# model = load_model("val05584_mem25_lstm1_dense2_unit256_dropout010_batch32.keras")
+# def calculate_selected_indicators(data):
+#     delta = data['Close'].diff()
+#     up, down = delta.clip(lower=0), -delta.clip(upper=0)
+#     ma_up = up.rolling(window=14).mean()
+#     ma_down = down.rolling(window=14).mean()
+#     data['RSI'] = 100 - (100 / (1 + ma_up / ma_down))
 
-    exp1 = data['Close'].ewm(span=12, adjust=False).mean()
-    exp2 = data['Close'].ewm(span=26, adjust=False).mean()
-    macd = exp1 - exp2
-    data['Macdhist'] = macd - macd.ewm(span=9, adjust=False).mean()
+#     exp1 = data['Close'].ewm(span=12, adjust=False).mean()
+#     exp2 = data['Close'].ewm(span=26, adjust=False).mean()
+#     macd = exp1 - exp2
+#     data['Macdhist'] = macd - macd.ewm(span=9, adjust=False).mean()
 
-    data['EMA_9'] = data['Close'].ewm(span=9, adjust=False).mean()
-    data['EMA_50'] = data['Close'].ewm(span=50, adjust=False).mean()
+#     data['EMA_9'] = data['Close'].ewm(span=9, adjust=False).mean()
+#     data['EMA_50'] = data['Close'].ewm(span=50, adjust=False).mean()
 
-    data.dropna(inplace=True)
-    return data
+#     data.dropna(inplace=True)
+#     return data
 
-class LSTMBasedStrategy(Strategy):
-    def init(self):
-        self.prediction = self.I(lambda x: x, full_predictions)
+# class LSTMBasedStrategy(Strategy):
+#     def init(self):
+#         self.prediction = self.I(lambda x: x, full_predictions)
 
-    def next(self):
-        if self.prediction[-1] == 1 and not self.position.is_long:
-            self.buy()
-        elif self.prediction[-1] == 0 and not self.position.is_short:
-            self.sell()
+#     def next(self):
+#         if self.prediction[-1] == 1 and not self.position.is_long:
+#             self.buy()
+#         elif self.prediction[-1] == 0 and not self.position.is_short:
+#             self.sell()
 
-stock = 'MSFT'
-startdate = "2023-01-15"
-enddate = "2024-01-15"
-mem_days = 25
-results_df = pd.DataFrame([])
+# stock = 'MSFT'
+# startdate = "2023-01-15"
+# enddate = "2024-01-15"
+# mem_days = 25
+# results_df = pd.DataFrame([])
 
-df = yf.download(stock, start=startdate, end=enddate, progress=False)
-df = calculate_selected_indicators(df)
-df['Label'] = (df['Close'].shift(-1) > df['Close']).astype(int)
-features = ['RSI', 'Macdhist', 'EMA_9', 'EMA_50', 'Volume']  
+# df = yf.download(stock, start=startdate, end=enddate, progress=False)
+# df = calculate_selected_indicators(df)
+# df['Label'] = (df['Close'].shift(-1) > df['Close']).astype(int)
+# features = ['RSI', 'Macdhist', 'EMA_9', 'EMA_50', 'Volume']  
 
-Backtest_scaler = StandardScaler()
-Backtest_scaler.fit(df[features])
-scaled_features = Backtest_scaler.transform(df[features])
+# Backtest_scaler = StandardScaler()
+# Backtest_scaler.fit(df[features])
+# scaled_features = Backtest_scaler.transform(df[features])
 
-X = np.array([scaled_features[i:i + mem_days] for i in range(len(scaled_features) - mem_days + 1)])
+# X = np.array([scaled_features[i:i + mem_days] for i in range(len(scaled_features) - mem_days + 1)])
 
-predictions = model.predict(X)
-predicted_classes = (predictions > 0.5).astype(int).flatten()
+# predictions = model.predict(X)
+# predicted_classes = (predictions > 0.5).astype(int).flatten()
 
-full_predictions = np.zeros(len(df))
-full_predictions[mem_days-1:mem_days-1+len(predicted_classes)] = predicted_classes
+# full_predictions = np.zeros(len(df))
+# full_predictions[mem_days-1:mem_days-1+len(predicted_classes)] = predicted_classes
 
-bt = Backtest(df, LSTMBasedStrategy, cash=10000, commission=.0425)
-results = bt.run()
-btplt = bt.plot()
+# bt = Backtest(df, LSTMBasedStrategy, cash=10000, commission=.0425)
+# results = bt.run()
+# btplt = bt.plot()
 
-st.bokeh_chart(btplt)
-
-
-
+# st.bokeh_chart(btplt)
